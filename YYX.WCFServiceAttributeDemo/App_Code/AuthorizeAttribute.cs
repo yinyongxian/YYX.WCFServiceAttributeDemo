c76f8f8d819e64717831f52ff51a2738d34ef44e
yinyongxian <yinyongxian@qq.com>
2021-08-10 07:51:03 +0800

    Add project files.

---
+++ YYX.WCFServiceAttributeDemo/App_Code/AuthorizeAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Web;
+
+/// <summary>
+/// Summary description for AuthorizeAttribute
+/// </summary>
+public class AuthorizeAttribute:Attribute, IServiceBehavior
+{
+    public AuthorizeAttribute()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ YYX.WCFServiceAttributeDemo/App_Code/MyParameterInspector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Dispatcher;
+using System.Web;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class MyParameterInspector :Attribute, IParameterInspector
+{
+    public MyParameterInspector ()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public void AfterCall(string operationName, object[] outputs, object returnValue, object correlationState)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object BeforeCall(string operationName, object[] inputs)
+    {
+        throw new NotImplementedException();
+    }
+}
